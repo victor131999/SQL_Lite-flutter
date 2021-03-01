4aedd41836be20655f38363435d5ecd713f9075e
victor131999 <tovir.gu.hu@hotmail.com>
2021-03-01 07:29:45 -0500

    Inicio de aplicación

---
+++ sql_lite/android/app/src/main/kotlin/com/cuyo/nrc3738/sql_lite/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.cuyo.nrc3738.sql_lite
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ sql_lite/lib/models/persons_model.dart
@@ -0,0 +1,9 @@
+class Person{
+  Person({this.id,this.identy,this.name,this.lastname,this.datebirth,this.disability});
+  int id;
+  String identy;
+  String name;
+  String lastname;
+  String datebirth;
+  String disability;
+}
